eaaab13b68f6b665d9861f8a3f437b0583162ff1
chaketek <chaketek@gmail.com>
2025-03-27 18:19:40 +0900

    add global ram definition

--- Firmware/src/dataregister.h
+++ Firmware/src/dataregister.h
@@ -122,7 +122,7 @@ typedef struct {
 // extern宣言
 // --------------------------------------------------------------------
 extern volatile CAN_data_t			g_CAN_data;
-extern volatile CALC_data_t			g_CALC_data;
+extern volatile CALC_data_t			g_CALC_data,g_CALC_data_sm;
 extern volatile unsigned long		sp_int, tr_int;
 extern volatile unsigned int		g_sw_cnt, g_sw_int_flg;
 
