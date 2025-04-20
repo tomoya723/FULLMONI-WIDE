2c155d2f031ad113e40515aeca12d7ccae3bdfac
tomoya723 <tomoya@violet.plala.or.jp>
2025-04-20 19:54:01 +0900

    update

--- Firmware/src/settings.h
+++ Firmware/src/settings.h
@@ -8,9 +8,19 @@
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
 
-#include "../aw/Source/Generated/Resource.h"
-#include "../aw/Source/Generated/ID_SCREEN_01.h"
-#include "../aw/Source/Generated/ID_SCREEN_02.h"
-#include "../aw/Source/Generated/ID_SCREEN_03.h"
+// emwin display resource number select
+#define DISP (1)  // 1-2
+
+#if (DISP == 1)
+	#include "../aw001/Source/Generated/Resource.h"
+	#include "../aw001/Source/Generated/ID_SCREEN_01.h"
+	#include "../aw001/Source/Generated/ID_SCREEN_02.h"
+	#include "../aw001/Source/Generated/ID_SCREEN_03.h"
+#elif (DISP == 2)
+	#include "../aw002/Source/Generated/Resource.h"
+	#include "../aw002/Source/Generated/ID_SCREEN_01.h"
+	#include "../aw002/Source/Generated/ID_SCREEN_02.h"
+	#include "../aw002/Source/Generated/ID_SCREEN_03.h"
+#endif
 
 #endif /* SETTINGS_H_ */
